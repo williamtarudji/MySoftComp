eddc81011f6e4798628d5cafd7a4ebd416822c25
loudyowen <44298280+loudyowen@users.noreply.github.com>
2018-10-19 23:07:00 +0700

    MySoftComp

---
+++ MySoftComp/resources/views/Home.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>MySoftComp</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<div class="Container">
+		<div class="Header">
+			<div class="Logo">
+				<img src="IMG/LOGO.png" width="300px" height="200px">
+			</div>
+		</div>
+		<div class="Content">
+			<div class="MainLogo">
+				<img src="IMG/LOGO.png">
+			</div>
+			<div class="Search">
+				<input type="search" name="Search-input" value="Search . . .">
+				<button>GO</button>
+			</div>
+		</div>
+		<div class="AdvancedSearch">
+			<a href="">Advanced</a>
+		</div>
+	</div>
+</body>
+</html>
