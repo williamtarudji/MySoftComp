52a618d3b4933b3f18d8b4be3fe6ce5e4ee72618
nugrohofigo <43537280+nugrohofigo@users.noreply.github.com>
2018-12-07 00:02:52 +0700

    Add views

---
+++ MySoftComp/resources/views/layout.blade.php
@@ -0,0 +1,44 @@
+<!doctype html>
+<html lang="en">
+  
+	<head>
+        @yield('head')
+		<title>MySoftComp @yield('title')</title>
+	</head>
+
+	<body>
+    <!-- Navigasi -->
+    <nav class="navbar navbar-expand-lg navbar-light bg-light sticky-top navigasi">
+      <a class="navbar-brand" href="#"><img src="../image/logo.png"></a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+       <div class="navbar-nav text-center">
+          <a class="nav-item nav-link" href="/home">Home</a>
+          <a class="nav-item nav-link" href="/howto">How To</a>
+          <a class="nav-item nav-link" href="/view_product">List</a>
+          <a class="nav-item nav-link" href="/compare">Compare</a>
+        </div>
+      </div>
+    </nav>
+    <!-- End Navigasi -->
+
+    <main>
+        @yield('main')
+    </main>
+
+    <!-- Footer -->
+    <footer class="my-5 pt-5 text-muted text-center text-small">
+        <p class="mb-1">&copy; 2018 MySoftComp @yield('footer')</p>
+    </footer>
+    {{-- End Footer --}}
+
+
+		<!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+	</body>
+</html>
---
+++ MySoftComp/resources/views/view_product.blade.php
@@ -0,0 +1,42 @@
+@extends('layout')
+@section('head')
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+	<!-- Bootstrap CSS -->
+  <link rel="stylesheet" type="text/css" href="{{asset('css/bootstrap.min.css')}}">
+	<link rel="stylesheet" type="text/css" href="{{asset('css/style_view_product.css')}}">
+@endsection
+@section('title')
+@section('main')
+    <div class="featurette">
+      <div class="row featurette feat">
+        <div class="col-md-6">
+          <h2 class="featurette-heading">First featurette heading. <span class="text-muted">It'll blow your mind.</span></h2>
+          <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+          <p class="lead">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quasi eaque, velit laborum atque error, nam dolorem nobis architecto necessitatibus, sed facilis consequuntur. Exercitationem placeat tenetur illum. Omnis dolores labore eaque!</p>
+        </div>
+        <div class="col-md-6">
+          <h2 class="featurette-heading">First featurette heading. <span class="text-muted">It'll blow your mind.</span></h2>
+          <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+          <p class="lead">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quasi eaque, velit laborum atque error, nam dolorem nobis architecto necessitatibus, sed facilis consequuntur. Exercitationem placeat tenetur illum. Omnis dolores labore eaque!</p>
+        </div>
+        <div class="col-md-6">
+          <h2 class="featurette-heading">First featurette heading. <span class="text-muted">It'll blow your mind.</span></h2>
+          <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+          <p class="lead">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quasi eaque, velit laborum atque error, nam dolorem nobis architecto necessitatibus, sed facilis consequuntur. Exercitationem placeat tenetur illum. Omnis dolores labore eaque!</p>
+        </div>
+        <div class="col-md-6">
+          <h2 class="featurette-heading">First featurette heading. <span class="text-muted">It'll blow your mind.</span></h2>
+          <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+          <p class="lead">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quasi eaque, velit laborum atque error, nam dolorem nobis architecto necessitatibus, sed facilis consequuntur. Exercitationem placeat tenetur illum. Omnis dolores labore eaque!</p>
+        </div>
+        <div class="col-md-6">
+          <h2 class="featurette-heading">First featurette heading. <span class="text-muted">It'll blow your mind.</span></h2>
+          <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+          <p class="lead">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quasi eaque, velit laborum atque error, nam dolorem nobis architecto necessitatibus, sed facilis consequuntur. Exercitationem placeat tenetur illum. Omnis dolores labore eaque!</p>
+        </div>
+      </div>
+    </div>
+@endsection
