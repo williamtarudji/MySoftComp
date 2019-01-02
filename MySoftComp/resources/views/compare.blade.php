edbd0c6223fe431c4532e3c98efbe7e9d8592074
nugrohofigo <43537280+nugrohofigo@users.noreply.github.com>
2019-01-02 20:37:30 +0700

    Update compare.blade.php

--- MySoftComp/resources/views/compare.blade.php
+++ MySoftComp/resources/views/compare.blade.php
@@ -6,13 +6,15 @@
 
 	<!-- Bootstrap CSS -->
     <link rel="stylesheet" type="text/css" href="{{asset('css/bootstrap.min.css')}}">
-	<link rel="stylesheet" type="text/css" href="{{asset('css/style_compare.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset('css/style_compare.css')}}">
+    <link rel="stylesheet" href="{{asset('intro.js-2.9.3/introjs.css')}}">
 @endsection
 @section('title')
 @section('main')
     <div class="content">
         <div class="dropdown category">
-            <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            <a class="btn btn-large btn-primary" href="javascript:void(0);" onclick="javascript:introJs().start();">Tour</a>
+            <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" data-step="1" data-intro="Halo, kamu bisa memilih kategori software disini">
                 Choose Category
             </button>
             <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
@@ -22,45 +24,45 @@
             </div>
         </div>
 
-        <div class="box-compare">
-            <div class="border">
+        <div class="box-compare" data-step="2" data-intro="Sekarang kita pindah ke bagian compare, disini kamu bisa memilih software yang akan dicompare" data-position='left'>
+            <div class="border" data-step="3" data-intro="Masukkan software yang akan dicompare" data-position='left'>
                 <div class="border-content">
                     <span>Software Name</span>
                     <div class="imagesoft">
                         <img src="image/logo.png" alt="">
                     </div>
                     <div class="dropcategory">
-                            <div class="itemBox">
-                                <form action="/" method="GET">
-                                    <input class="itemBox1" type="text" placeholder="Software1..." name="item1">
-                                </form>
-                            </div>
+                        <div class="itemBox">
+                            <form action="/" method="GET">
+                                <input class="itemBox1" type="text" placeholder="Software1..." name="item1">
+                            </form>
+                        </div>
                     </div>
                 </div>
             </div>
             
-            <div class="icon">
+            <div class="icon" data-step="5" data-intro="Terakhir tekan tombol ini untuk melihat hasil compare softwaremu">
                 <a href="">
-                    <img src="image/icon-search.png" alt="">
+                    <img src="image/graph.png" alt="">
                 </a>    
             </div>
 
-            <div class="border">
+            <div class="border" data-step="4" data-intro="Sekarang, kamu bisa pilih satu software lagi sebagai perbandingan" data-position='right'>
                 <div class="border-content">
                     <span>Software Name</span>
                     <div class="imagesoft">
                         <img src="image/logo.png" alt="">
                     </div>
                     <div class="dropcategory">
-
-                            <div class="itemBox">
-                                <form action="/" method="GET">
-                                    <input class="itemBox2" type="text" placeholder="Software2..." name="item2">
-                                </form>
-                            </div>
+                        <div class="itemBox">
+                            <form action="/" method="GET">
+                                <input class="itemBox2" type="text" placeholder="Software2..." name="item2">
+                            </form>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
+    <script src="{{asset('intro.js-2.9.3/intro.js')}}"></script>
 @endsection
