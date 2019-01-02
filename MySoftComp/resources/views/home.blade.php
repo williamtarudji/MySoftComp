6c5e3e7986183239461c600e8cffbbec490ef7ed
nugrohofigo <43537280+nugrohofigo@users.noreply.github.com>
2019-01-02 20:36:50 +0700

    Update home.blade.php

--- MySoftComp/resources/views/home.blade.php
+++ MySoftComp/resources/views/home.blade.php
@@ -16,8 +16,8 @@
     <div class="container">
       <h1 class="display-4">MySoftComp</h1>
       <h6 class="display-4 text-left intro">Merupakan website yang dapat membantu anda dalam memilih software yang sesuai kebutuhan anda</h6>
+      <a href="/compare"><button type="button" class="btn btn-primary">Compare</button></a>
     </div>
-      <button id="buttonCompare"><a href="/compare"> COMPARE</a></button>
   </div>
   <!-- End Jumbotron -->
 
@@ -26,48 +26,32 @@
   <div class="featurette">
     <div class="row featurette feat">
       <div class="col-md-5 textright">
-        <h2 class="featurette-heading headtext">Pilih category software yang ingin anda bandingkan.</h2>
+        <h2 class="featurette-heading headtext">First featurette heading. <span class="text-muted">It'll blow your mind.</span></h2>
+        <p class="lead textright">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
       </div>
       <div class="col-md-4 imagefeat">
-        <img src="../image/howTo/howTo1.jpg" alt="" class="rounded">
+        <img src="../image/home_1.jpg" alt="" class="rounded">
       </div>
     </div>
     <hr class="featurette-divider">
     <div class="row featurette feat">
       <div class="col-md-4 imagefeat1">
-        <img src="../image/howTo/howTo2.jpg" class="rounded">
+        <img src="../image/home_2.jpeg" class="rounded">
       </div>
       <div class="col-md-5 textleft">
-          <h2 class="featurette-heading headtext">Masukkan nama software dari kategori yang anda ingin bandingkan.</h2>
+        <h2 class="featurette-heading headtext">First featurette heading. <span class="text-muted">It'll blow your mind.</span></h2>
+        <p class="lead textleft">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
       </div>
     </div>
     <hr class="featurette-divider">
       <div class="row featurette feat">
         <div class="col-md-5 textright">
-            <h2 class="featurette-heading headtext">Tekan tombol search dan MySoftComp akan menampilkan perbandingan software yang ingin dibandingkan.</h2>
+          <h2 class="featurette-heading headtext">First featurette heading. <span class="text-muted">It'll blow your mind.</span></h2>
+          <p class="lead textright">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
         </div>
         <div class="col-md-4 imagefeat">
-          <img src="../image/howTo/howTo3.jpg" alt="" class="rounded">
+          <img src="../image/picture1.png" alt="" class="rounded">
         </div>
       </div>
   </div>
-
-  <script>
-      $(function(){
-          $('a[href*="#"]:not([href="#"])').click(function(){
-              if(location.pathname.replace(/^\//,'')==this.pathname.replace(/^\//'')&&location.hostname == this.hostname)
-              {
-                  var target = $(this.hash);
-                  target=target.length?target:$('[name'+this.hash.slice(1)+']');
-                  if(target.length){
-                      $('html,body').animate({
-                          scrollTop: target.offset().top
-                      },1000);
-                      return false;
-                  }
-              }
-          });
-      });
-  </script>
-
 @endsection
